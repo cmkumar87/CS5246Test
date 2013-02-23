3999a25c60338cf09b7d470cd2e4434036d8b7c8
Shaun Lee <lee.jwsh@gmail.com>
2013-02-23 11:35:30 +0800

    CS5246 Test

---
+++ src/NERDemo.java
@@ -0,0 +1,100 @@
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+
+
+public class NERDemo
+{
+	public static void main (String[] args) throws IOException
+	{
+		String serializedClassifier = "NERclassifiers/english.all.3class.distsim.crf.ser.gz";
+		
+		// usage: java NERDemo <classifier-file> <file-to-classify> <output-file>
+		// or
+		// change the hardcoded string(s) in the else section (default classifier is english.all.3class.distsim)
+		
+		if (args.length > 0)
+		{
+			serializedClassifier = args[0];
+		}
+		
+		AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
+		
+		if (args.length > 1)
+		{
+			/*
+			System.out.println(args[1]);
+			String fileContents = IOUtils.slurpFile(args[1]);
+			java.util.List<java.util.List<CoreLabel>> out = classifier.classify(fileContents);
+			for (java.util.List<CoreLabel> sentence : out)
+			{
+				for (CoreLabel word : sentence)
+				{
+					System.out.print(word.word() + '/' + word.get(AnswerAnnotation.class) + ' ');
+				}
+				System.out.println();
+			}
+			out = classifier.classifyFile(args[1]);
+			for (java.util.List<CoreLabel> sentence : out)
+			{
+				for (CoreLabel word : sentence)
+				{
+					System.out.print(word.word() + '/' + word.get(AnswerAnnotation.class) + ' ');
+				}
+				System.out.println();
+			}
+			*/
+			
+			if (args.length > 2)
+			{
+				FileWriter fileOut = new FileWriter(args[2]);
+				BufferedWriter writer = new BufferedWriter(fileOut);
+				writer.write("");
+				writer.close();
+			}
+			
+			FileReader fileIn = new FileReader(args[1]);
+			Scanner sc = new Scanner(fileIn);
+			while (sc.hasNextLine())
+			{
+				String sentence = sc.nextLine();
+				System.out.println(classifier.classifyToString(sentence));
+				
+				if (args.length > 2)
+				{
+					FileWriter fileOut = new FileWriter(args[2], true);
+					BufferedWriter writer = new BufferedWriter(fileOut);
+					writer.write(classifier.classifyToString(sentence) + "\n");
+					writer.close();
+				}
+			}
+		}
+		else
+		{
+			String s1 = "Onthology Tutorial in Java";
+			//String s2 = "albert einstein was born in germany.";
+			System.out.println(classifier.classifyToString(s1));
+			/*
+			System.out.println(classifier.classifyWithInlineXML(s2));
+			System.out.println(classifier.classifyToString(s2, "xml", true));
+			int i = 0;
+			for (java.util.List<CoreLabel> lcl : classifier.classify(s2))
+			{
+				for (CoreLabel cl : lcl)
+				{
+					System.out.println(i++ + ":");
+					System.out.println(cl);
+				}
+			}
+			*/
+		}
+	}
+}
---
+++ src/TaggerDemo.java
@@ -0,0 +1,75 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+public class TaggerDemo
+{
+	public static void main(String[] args) throws Exception
+	{
+		String modelFile = "POSmodels/english-caseless-left3words-distsim.tagger";
+		
+		// usage: java TaggerDemo <model-file> <file-to-tag> <output-file>
+		// or
+		// change the hardcoded string(s) in the else section (default model is english-caseless-left3words-distsim)
+		
+		if (args.length > 0)
+		{
+			modelFile = args[0];
+		}
+		
+		MaxentTagger tagger = new MaxentTagger(modelFile);
+		
+		if (args.length > 1)
+		{
+			/*
+			List<List<HasWord>> sentences = MaxentTagger.tokenizeText(new BufferedReader(new FileReader(args[1])));
+			for (List<HasWord> sentence : sentences)
+			{
+				ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
+				System.out.println(Sentence.listToString(tSentence, false));
+			}
+			*/
+			
+			if (args.length > 2)
+			{
+				FileWriter fileOut = new FileWriter(args[2]);
+				BufferedWriter writer = new BufferedWriter(fileOut);
+				writer.write("");
+				writer.close();
+			}
+			
+			FileReader fileIn = new FileReader(args[1]);
+			Scanner sc = new Scanner(fileIn);
+			while (sc.hasNextLine())
+			{
+				String sentence = sc.nextLine();
+				System.out.println(tagger.tagString(sentence));
+				
+				if (args.length > 2)
+				{
+					FileWriter fileOut = new FileWriter(args[2], true);
+					BufferedWriter writer = new BufferedWriter(fileOut);
+					writer.write(tagger.tagString(sentence) + "\n");
+					writer.close();
+				}
+			}
+		}
+		else
+		{
+			String s = "onthology tutorial in java";
+			System.out.println(tagger.tagString(s));
+		}
+	}
+}
